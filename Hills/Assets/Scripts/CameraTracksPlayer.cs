c636c5cbc2c34b52eee690a4331729288231f8ba
Dynamikey <mr_michaelzhao@hotmail.com>
2017-08-15 23:26:55 +0100

    Camera tracks the player movements

---
+++ Hills/Assets/Scripts/CameraTracksPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracksPlayer : MonoBehaviour {
+
+	Transform playerTransform;
+	float offsetX;
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+	
+		offsetX = transform.position.x - playerTransform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerTransform!= null) {
+			Vector3 pos = transform.position;
+			pos.x = offsetX + playerTransform.position.x;
+			transform.position = pos;
+		}
+
+	}
+}
